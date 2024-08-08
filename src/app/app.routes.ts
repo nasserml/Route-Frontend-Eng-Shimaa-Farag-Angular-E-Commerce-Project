b7770bcf28fbacd88b823356c6a06e6ecf3d6415
Mahmoud Nasser <mnasserone@gmail.com>
2024-08-08 15:24:49 +0300

    Week-4-session-1 4-Routing routes added in the app.route.ts  and categories component added in layout/pages folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layout/pages/home/home.component';
+import { CartComponent } from './layout/pages/cart/cart.component';
+import { BrandComponent } from './layout/pages/brand/brand.component';
+import { ProductsComponent } from './layout/pages/products/products.component';
+import { CategoriesComponent } from './layout/pages/categories/categories.component';
+import { NotfoundComponent } from './layout/additions/notfound/notfound.component';
+import { LoginComponent } from './layout/pages/login/login.component';
+import { RegisterComponent } from './layout/pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', component: HomeComponent},
+    {path:'cart', component: CartComponent},
+    {path:'brands', component: BrandComponent},
+    {path:'products', component: ProductsComponent},
+    {path:'categories', component: CategoriesComponent},
+    {path:'login', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
+    {path:'**', component: NotfoundComponent}
+ 
+];
