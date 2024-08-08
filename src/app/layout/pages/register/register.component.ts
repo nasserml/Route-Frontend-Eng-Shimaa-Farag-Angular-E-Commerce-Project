702f8af9f2e419144f13ccda7b75fd7d03e6dc3e
Mahmoud Nasser <mnasserone@gmail.com>
2024-08-08 20:01:02 +0300

    Week-4-session-1 10-validation 11-show-alert-message 12-loadingand-diable-button added to the register component ts and it html

--- src/app/layout/pages/register/register.component.ts
+++ src/app/layout/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+import {ReactiveFormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
 import { AuthService } from '../../../shared/services/auth.service';
 import { Router } from '@angular/router';
 
@@ -12,12 +12,15 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
+  errMsg:string ='';
+  isLoading:boolean = false;
+
   registerForm:FormGroup = new FormGroup({
-    name:new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    rePassword: new FormControl(null),
-    phone: new FormControl(null)
+    name:new FormControl(null,[Validators.required, Validators.minLength(3),Validators.maxLength(10)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{8}$/)]),
+    rePassword: new FormControl(null, [Validators.required, ]),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[1250][0-9]{8}$/)])
 
   });
 
@@ -25,23 +28,29 @@ export class RegisterComponent {
 
 
   sendData():void{
+    this.isLoading= true;
+
+    console.log(this.registerForm)
     
+
     this._auth.register(this.registerForm.value).subscribe({
       next: (response) => {
         if(response.message == 'success') {
 
-          this.router.navigate(['/login'])
+          this.router.navigate(['/login']);
+          this.isLoading= false;
 
         }
       },
       error: (err) => {
         console.log('error',err);
+        this.errMsg = err.error.message;
+        this.isLoading = false
       },
       complete: () =>{
         console.log('complete')
       }
-    })
-
+    });
   }
 
 }
