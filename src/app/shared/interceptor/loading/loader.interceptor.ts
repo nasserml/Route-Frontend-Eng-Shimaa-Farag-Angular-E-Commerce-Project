244082a7823da144e95f6a982731109b407592b8
Mahmoud Nasser <mnasserone@gmail.com>
2024-08-29 16:20:47 +0300

    Week-6-session-2-6-interceptor-with-loading-scrren added

--- src/app/shared/interceptor/loading/loader.interceptor.ts
+++ src/app/shared/interceptor/loading/loader.interceptor.ts
@@ -1,5 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { finalize } from 'rxjs';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  const _ngxSpinner = inject(NgxSpinnerService);
+
+  _ngxSpinner.show();
+
+  return next(req).pipe(finalize(() => _ngxSpinner.hide()));
 };
