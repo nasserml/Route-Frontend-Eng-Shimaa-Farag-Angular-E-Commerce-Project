0411d222bb7341952ab643c21d757e34231c85b5
Mahmoud Nasser <mnasserone@gmail.com>
2024-09-02 15:38:59 +0300

    Week-7-session-1-2-lazy-loading-with-standalone-component  added to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './layout/pages/home/home.component';
-import { CartComponent } from './layout/pages/cart/cart.component';
-import { BrandComponent } from './layout/pages/brand/brand.component';
-import { ProductsComponent } from './layout/pages/products/products.component';
-import { CategoriesComponent } from './layout/pages/categories/categories.component';
-import { NotfoundComponent } from './layout/additions/notfound/notfound.component';
-import { LoginComponent } from './layout/pages/login/login.component';
-import { RegisterComponent } from './layout/pages/register/register.component';
 import { autGuard } from './shared/guard/aut.guard';
-import { ProductDetailsComponent } from './layout/additions/product-details/product-details.component';
-import { CheckoutComponent } from './layout/pages/checkout/checkout.component';
-import { AllOrdersComponent } from './layout/pages/all-orders/all-orders.component';
-import { ForgetPasswordComponent } from './layout/pages/forget-password/forget-password.component';
 import { logoutGuard } from './shared/guard/logout.guard';
-import { ProductsSearchComponent } from './layout/pages/products-search/products-search.component';
-import { WishListComponent } from './layout/pages/wish-list/wish-list.component';
 import { cartGuard } from './shared/guard/cart.guard';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},
-    {path:'home', component: HomeComponent, canActivate:[autGuard], title:'Home'},
-    {path:'cart', component: CartComponent, canActivate:[autGuard], title:'Cart'},
-    {path:'brands', component: BrandComponent, canActivate:[autGuard], title:'Brands' },
-    {path:'products', component: ProductsSearchComponent, canActivate:[autGuard], title:'Products'},
-    {path:'productDetails/:productId', component: ProductDetailsComponent, canActivate:[autGuard]},
-    {path:'categories', component: CategoriesComponent, canActivate:[autGuard], title:'Categories'},
-    {path:'checkout', component: CheckoutComponent, canActivate:[autGuard, cartGuard], title:'Check Out'},
-    {path:'allorders', component: AllOrdersComponent, canActivate:[autGuard], title:'All Orders'},
-    {path:'wishlist', component: WishListComponent, canActivate:[autGuard], title:'Wish List'},
-    {path:'login', component: LoginComponent,title:'Login', canActivate: [logoutGuard]},
-    {path:'forget-password', component: ForgetPasswordComponent,title:'Forget Password', canActivate: [logoutGuard]},
-    {path:'register', component: RegisterComponent, title:'Register', canActivate:[logoutGuard]},
-    {path:'**', component: NotfoundComponent, title:'Not Found'},
+    {path:'home', loadComponent: ()=>import('./layout/pages/home/home.component').then(c=>c.HomeComponent), canActivate:[autGuard], title:'Home'},
+    {path:'cart', loadComponent: ()=>import('./layout/pages/cart/cart.component').then(c=>c.CartComponent), canActivate:[autGuard], title:'Cart'},
+    {path:'brands', loadComponent: ()=>import('./layout/pages/brand/brand.component').then(c=>c.BrandComponent), canActivate:[autGuard], title:'Brands' },
+    {path:'products',loadComponent:()=>import('./layout/pages/products/products.component').then(c=>c.ProductsComponent), canActivate:[autGuard], title:'Products'},
+    {path:'productDetails/:productId', loadComponent: ()=>import('./layout/additions/product-details/product-details.component').then(c=>c.ProductDetailsComponent), canActivate:[autGuard]},
+    {path:'categories', loadComponent: ()=>import('./layout/pages/categories/categories.component').then(c=>c.CategoriesComponent), canActivate:[autGuard], title:'Categories'},
+    {path:'checkout', loadComponent: ()=>import('./layout/pages/checkout/checkout.component').then(c=>c.CheckoutComponent), canActivate:[autGuard, cartGuard], title:'Check Out'},
+    {path:'allorders', loadComponent: ()=>import('./layout/pages/all-orders/all-orders.component').then(c=>c.AllOrdersComponent), canActivate:[autGuard], title:'All Orders'},
+    {path:'wishlist', loadComponent: ()=>import('./layout/pages/wish-list/wish-list.component').then(c=>c.WishListComponent), canActivate:[autGuard], title:'Wish List'},
+    {path:'login', loadComponent: ()=>import('./layout/pages/login/login.component').then(c=>c.LoginComponent),title:'Login', canActivate: [logoutGuard]},
+    {path:'forget-password', loadComponent: ()=>import('./layout/pages/forget-password/forget-password.component').then(c=>c.ForgetPasswordComponent),title:'Forget Password', canActivate: [logoutGuard]},
+    {path:'register', loadComponent: ()=>import('./layout/pages/register/register.component').then(c=>c.RegisterComponent), title:'Register', canActivate:[logoutGuard]},
+    {path:'**', loadComponent: ()=>import('./layout/additions/notfound/notfound.component').then(c=>c.NotfoundComponent), title:'Not Found'},
  
 ];
--- src/app/shared/interceptor/error/error.interceptor.ts
+++ src/app/shared/interceptor/error/error.interceptor.ts
@@ -7,7 +7,7 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toastrService= inject(ToastrService);
 
   return next(req).pipe(catchError(error=> {
-    toastrService.error('Something went wrong! Please try again later');
+    // toastrService.error('Something went wrong! Please try again later');
 
     return throwError(()=> error)
   }));
